f3913d0012599fcd26ff3958a986e4a61e142fd3
Peter LeVasseur <peter.levasseur@gm.com>
2018-04-09 15:07:06 -0400

    Filled in the rest(?) of the necessary members of the KalmanFilter.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
-#![feature(const_fn)]
 #![feature(proc_macro)]
-#![feature(unsize)]
 #![no_std]
 
 pub extern crate mat;
@@ -10,106 +8,124 @@ pub extern crate generic_array;
 use core::ops::{Mul};
 use core::fmt;
 
-use mat::{Mat, MatGen, MatGen2};
-use mat::mat;
-use mat::traits::{Matrix, Zero};
-use typenum::{Unsigned, Prod, UInt, UTerm};
+use mat::traits::{Zero};
+use typenum::{Unsigned, Prod};
 use typenum::consts::*;
-use generic_array::{GenericArray, ArrayLength, arr, arr_impl};
+use generic_array::{ArrayLength};
 
-
-#[derive(Clone)]
-pub struct KalmanFilterMat<T, DP>
-    where
-        T: Copy + Zero + Default,
-        DP: Unsigned,
-//    MP: Unsigned,
-//    CP: Unsigned
-{
-    state_pre: Mat<T, [T; 1], DP, U1>
-}
-
-impl<T, DP> KalmanFilterMat<T, DP>
-    where
-        T: Copy + Zero + Default,
-        DP: Unsigned,
-{
-//    fn init() -> KalmanFilterMat<T, DP> {
-//            // how to initialize
-//    }
-}
-
-pub struct KalmanFilterMatGen2<T, DP>
+pub struct KalmanFilter<T, DP, MP, CP>
 where
     T: Copy + Zero + Default,
     DP: Unsigned,
+    MP: Unsigned,
+    CP: Unsigned,
     DP: Mul<U1>,
     Prod<DP, U1>: ArrayLength<T>,
+    DP: Mul<DP>,
+    Prod<DP, DP>: ArrayLength<T>,
+    MP: Mul<DP>,
+    Prod<MP, DP>: ArrayLength<T>,
+    MP: Mul<MP>,
+    Prod<MP, MP>: ArrayLength<T>,
+    DP: Mul<MP>,
+    Prod<DP, MP>: ArrayLength<T>,
+    DP: Mul<CP>,
+    Prod<DP, CP>: ArrayLength<T>
 {
-    state_pre: MatGen2<T, DP, U1>
+    state_pre: mat::MatGen<T, DP, U1>,
+    state_post: mat::MatGen<T, DP, U1>,
+    transition_matrix: mat::MatGen<T, DP, DP>,
+
+    process_noise_cov: mat::MatGen<T, DP, DP>,
+    measurement_matrix: mat::MatGen<T, MP, DP>,
+    measurement_noise_cov: mat::MatGen<T, MP, MP>,
+
+    error_cov_pre: mat::MatGen<T, DP, DP>,
+    error_cov_post: mat::MatGen<T, DP, DP>,
+    gain: mat::MatGen<T, DP, MP>,
+
+    control_matrix: mat::MatGen<T, DP, CP>
 }
 
-impl<T, DP> KalmanFilterMatGen2<T, DP>
+impl<T, DP, MP, CP> KalmanFilter<T, DP, MP, CP>
 where
     T: Copy + Zero + Default,
     DP: Unsigned,
+    MP: Unsigned,
+    CP: Unsigned,
     DP: Mul<U1>,
     Prod<DP, U1>: ArrayLength<T>,
+    DP: Mul<DP>,
+    Prod<DP, DP>: ArrayLength<T>,
+    MP: Mul<DP>,
+    Prod<MP, DP>: ArrayLength<T>,
+    MP: Mul<MP>,
+    Prod<MP, MP>: ArrayLength<T>,
+    DP: Mul<MP>,
+    Prod<DP, MP>: ArrayLength<T>,
+    DP: Mul<CP>,
+    Prod<DP, CP>: ArrayLength<T>
 {
     pub fn init() -> Self {
-        KalmanFilterMatGen2 {
-            state_pre: Default::default()
+        KalmanFilter {
+            state_pre: Default::default(),
+            state_post: Default::default(),
+            transition_matrix: Default::default(),
+
+            process_noise_cov: Default::default(),
+            measurement_matrix: Default::default(),
+            measurement_noise_cov: Default::default(),
+
+            error_cov_pre: Default::default(),
+            error_cov_post: Default::default(),
+            gain: Default::default(),
+
+            control_matrix: Default::default()
         }
     }
 }
 
-impl<T, DP> fmt::Debug for KalmanFilterMatGen2<T, DP>
+impl<T, DP, MP, CP> fmt::Debug for KalmanFilter<T, DP, MP, CP>
 where
     T: Copy + Zero + Default + fmt::Debug,
     DP: Unsigned,
+    MP: Unsigned,
+    CP: Unsigned,
     DP: Mul<U1>,
     Prod<DP, U1>: ArrayLength<T>,
+    DP: Mul<DP>,
+    Prod<DP, DP>: ArrayLength<T>,
+    MP: Mul<DP>,
+    Prod<MP, DP>: ArrayLength<T>,
+    MP: Mul<MP>,
+    Prod<MP, MP>: ArrayLength<T>,
+    DP: Mul<MP>,
+    Prod<DP, MP>: ArrayLength<T>,
+    DP: Mul<CP>,
+    Prod<DP, CP>: ArrayLength<T>
 {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         f.write_str("state_pre: ")?;
-        write!(f, "{:?}", self.state_pre)?;
-        f.write_str("]")
+        write!(f, "{:?}; ", self.state_pre)?;
+        f.write_str("state_post: ")?;
+        write!(f, "{:?}; ", self.state_post)?;
+        f.write_str("transition_matrix: ")?;
+        write!(f, "{:?}; ", self.transition_matrix)?;
+        f.write_str("process_noise_cov: ")?;
+        write!(f, "{:?}; ", self.process_noise_cov)?;
+        f.write_str("measurement_matrix: ")?;
+        write!(f, "{:?}; ", self.measurement_matrix)?;
+        f.write_str("measurement_noise_cov: ")?;
+        write!(f, "{:?}; ", self.measurement_noise_cov)?;
+        f.write_str("error_cov_pre: ")?;
+        write!(f, "{:?}; ", self.error_cov_pre)?;
+        f.write_str("error_cov_post: ")?;
+        write!(f, "{:?}; ", self.error_cov_post)?;
+        f.write_str("gain: ")?;
+        write!(f, "{:?}; ", self.gain)?;
+        f.write_str("control_matrix: ")?;
+        write!(f, "{:?}; ", self.control_matrix)
     }
-}
-
-fn build_matrix() {
-
-    // 2 by 3 matrix
-    let a = mat![
-        [1, 2, 3],
-        [3, 4, 5],
-    ];
-
-    // 3 by 2 matrix
-    let b = mat![
-        [1, 2],
-        [3, 4],
-        [5, 6],
-    ];
-
-    // build an expression tree
-    let c = &a * &b;
-
-    let a: Mat<i32, [i32; 2], U2, U1> = unsafe {
-        Mat::new([0; 2])
-    };
-
-    let a = arr![u32; 1, 2, 3];
-
-    let b : GenericArray<u32, U2> = Default::default();
-
-    type G = typenum::Sum<U0, U1>;
-    G::to_u32();
-
-    type MYU1 = U1;
-
-    // partially evaluate the tree
-    assert_eq!(c.get(0, 0), 22);
 }
 
 #[cfg(test)]
