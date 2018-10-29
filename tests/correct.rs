0befcd1e3ba0af538eabeb2b8ced5a3dfa31e500
Peter LeVasseur <plevasseur@gmail.com>
2018-10-29 18:59:23 -0400

    Specified tests for f32 and f64.

--- tests/correct.rs
+++ tests/correct.rs
@@ -2,7 +2,7 @@ extern crate rudie;
 extern crate assert as asrt;
 
 #[test]
-fn correct() {
+fn single_correct_f64() {
     use rudie::KalmanFilter;
     use rudie::na::{U0, U1, U2, Vector, MatrixArray};
     use asrt::close;
@@ -28,6 +28,52 @@ fn correct() {
     kf.measurement_noise_cov = rudie::na::Vector1::new(0.5);
 
     let measurement: Vector<f64, U1, MatrixArray<f64, U1, U1>> = rudie::na::Vector1::new(2.0);
+
+    kf.correct(measurement);
+
+    close(kf.residual[(0,0)], -3., epsilon);
+
+    close(kf.innov_cov[(0,0)], 13.5, epsilon);
+
+    close(kf.gain[(0,0)], 5./13.5, epsilon);
+    close(kf.gain[(1,0)], 4./13.5, epsilon);
+
+    close(kf.state_post[(0,0)], -1.5/13.5, epsilon);
+    close(kf.state_post[(1,0)], 15./13.5, epsilon);
+
+    close(kf.error_cov_post[(0,0)], -11.5/13.5, epsilon);
+    close(kf.error_cov_post[(0,1)], 7./13.5, epsilon);
+    close(kf.error_cov_post[(1,0)], 7./13.5, epsilon);
+    close(kf.error_cov_post[(1,1)], -2.5/13.5, epsilon);
+}
+
+#[test]
+fn single_correct_f32() {
+    use rudie::KalmanFilter;
+    use rudie::na::{U0, U1, U2, Vector, MatrixArray};
+    use asrt::close;
+
+    let epsilon = 0.0000001;
+
+    let mut kf: KalmanFilter<f32, U2, U1, U0> = KalmanFilter::init();
+
+    kf.measurement_matrix = rudie::na::Matrix1x2::new(
+        1., 2.
+    );
+
+    kf.state_pre = rudie::na::Matrix2x1::new(
+        1.,
+        2.
+    );
+
+    kf.error_cov_pre = rudie::na::Matrix2::new(
+        1., 2.,
+        2., 1.
+    );
+
+    kf.measurement_noise_cov = rudie::na::Vector1::new(0.5);
+
+    let measurement: Vector<f32, U1, MatrixArray<f32, U1, U1>> = rudie::na::Vector1::new(2.0);
 
     kf.correct(measurement);
 
--- tests/predict.rs
+++ tests/predict.rs
@@ -1,7 +1,7 @@
 extern crate rudie;
 
 #[test]
-fn predict_dummy_control() {
+fn single_predict_dummy_control_f64() {
     use rudie::KalmanFilter;
     use rudie::na::{U0, U1, U2, Vector, MatrixArray, zero};
 
@@ -26,13 +26,67 @@ fn predict_dummy_control() {
 }
 
 #[test]
-fn predict_with_control() {
+fn single_predict_with_control_f64() {
     use rudie::KalmanFilter;
     use rudie::na::{U1, U2, Vector, MatrixArray};
 
     let dummy_control: Vector<f64, U1, MatrixArray<f64, U1, U1>> = rudie::na::Vector1::new(1.0);
 
     let mut kf: KalmanFilter<f64, U2, U1, U1> = KalmanFilter::init();
+    kf.control_matrix = rudie::na::Matrix2x1::new(
+        1.,
+        1.
+    );
+
+    kf.state_post = rudie::na::Matrix2x1::new(
+        1.,
+        2.
+    );
+
+    kf.transition_matrix = rudie::na::Matrix2::new(
+        2., 3.,
+        4., 5.
+    );
+
+    kf.predict(dummy_control);
+
+    assert_eq!(kf.state_pre[(0,0)], 9.);
+    assert_eq!(kf.state_pre[(1,0)], 15.);
+}
+
+#[test]
+fn single_predict_dummy_control_f32() {
+    use rudie::KalmanFilter;
+    use rudie::na::{U0, U1, U2, Vector, MatrixArray, zero};
+
+    let dummy_control: Vector<f32, U0, MatrixArray<f32, U0, U1>> = zero();
+    // no control parameters, CP = U0
+    let mut kf: KalmanFilter<f32, U2, U1, U0> = KalmanFilter::init();
+
+    kf.state_post = rudie::na::Matrix2x1::new(
+        1.,
+        2.
+    );
+
+    kf.transition_matrix = rudie::na::Matrix2::new(
+        2., 3.,
+        4., 5.
+    );
+
+    kf.predict(dummy_control);
+
+    assert_eq!(kf.state_pre[(0,0)], 8.);
+    assert_eq!(kf.state_pre[(1,0)], 14.);
+}
+
+#[test]
+fn single_predict_with_control_f32() {
+    use rudie::KalmanFilter;
+    use rudie::na::{U1, U2, Vector, MatrixArray};
+
+    let dummy_control: Vector<f32, U1, MatrixArray<f32, U1, U1>> = rudie::na::Vector1::new(1.0);
+
+    let mut kf: KalmanFilter<f32, U2, U1, U1> = KalmanFilter::init();
     kf.control_matrix = rudie::na::Matrix2x1::new(
         1.,
         1.
